28044ceefe68411f5547ad707a5b20b942f5f4c0
Kostika Nini <ninik@sheridancollege.ca~>
2024-05-28 20:48:50 -0400

    dropdown fixed

--- Eagles_Shoe_Website/src/main/resources/static/Script/dropdown.js
+++ Eagles_Shoe_Website/src/main/resources/static/Script/dropdown.js
@@ -2,31 +2,78 @@
  * 
  */
 
+const elements31 = document.querySelectorAll("[class$='-dropdown']");
+elements31.forEach(element => {
+element.addEventListener("mouseover", function(){
+	                switch(element.className) {
+			case "men-dropdown":
+                document.querySelector(".men-dropdown").style.display = "flex";
+                break;
+            case "boys-dropdown":
+                document.querySelector(".boys-dropdown").style.display = "flex";
+                break;
+            case "girls-dropdown":
+                document.querySelector(".girls-dropdown").style.display = "flex";
+                break;
+            case "sales-dropdown":
+                document.querySelector(".sales-dropdown").style.display = "flex";
+                break;
+            case "sport-dropdown":
+                document.querySelector(".sport-dropdown").style.display = "flex";
+                break;
+            case "women-dropdown":
+                document.querySelector(".women-dropdown").style.display = "flex";
+                break;						}
+})
+});
+
+elements31.forEach(element => {
+element.addEventListener("mouseout", function(){
+	                switch(element.className) {
+			case "men-dropdown":
+                document.querySelector(".men-dropdown").style.display = "none";
+                break;
+            case "boys-dropdown":
+                document.querySelector(".boys-dropdown").style.display = "none";
+                break;
+            case "girls-dropdown":
+                document.querySelector(".girls-dropdown").style.display = "none";
+                break;
+            case "sales-dropdown":
+                document.querySelector(".sales-dropdown").style.display = "none";
+                break;
+            case "sport-dropdown":
+                document.querySelector(".sport-dropdown").style.display = "none";
+                break;
+            case "women-dropdown":
+                document.querySelector(".women-dropdown").style.display = "none";
+                break;						}
+})
+});
+
+
 
 const elements = document.querySelectorAll(".dropdown");
 	elements.forEach(element => {
     element.addEventListener("mouseover", function() {
         switch(element.innerHTML) {
             case "Men":
-                document.querySelector(".men-dropdown").style.display = "block";
+                document.querySelector(".men-dropdown").style.display = "flex";
                 break;
             case "Boys":
-                document.querySelector(".boys-dropdown").style.display = "block";
+                document.querySelector(".boys-dropdown").style.display = "flex";
                 break;
             case "Girls":
-                document.querySelector(".girls-dropdown").style.display = "block";
+                document.querySelector(".girls-dropdown").style.display = "flex";
                 break;
             case "Sales":
-                document.querySelector(".sales-dropdown").style.display = "block";
+                document.querySelector(".sales-dropdown").style.display = "flex";
                 break;
             case "Sport":
-                document.querySelector(".sport-dropdown").style.display = "block";
+                document.querySelector(".sport-dropdown").style.display = "flex";
                 break;
             case "Women":
-                document.querySelector(".women-dropdown").style.display = "block";
-                break;
-            default:
-                // Handle default case if needed
+                document.querySelector(".women-dropdown").style.display = "flex";
                 break;
         }
     });
