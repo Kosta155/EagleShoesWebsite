51c23ded25f8766b336516c75009a74a2dd25e23
Fuwad_Oladega <122299902+FuwadOladega@users.noreply.github.com>
2024-08-24 22:37:07 -0400

    Search

--- Eagles_Shoe_Website/src/main/resources/static/Script/modal.js
+++ Eagles_Shoe_Website/src/main/resources/static/Script/modal.js
@@ -21,3 +21,24 @@ cancel.forEach(can => {
         document.documentElement.style.overflow = 'auto';
     });
 });
+
+const search = document.querySelector("#searchbar");
+const overlay1 = document.querySelector(".overlay1");
+const modal1 = document.querySelector(".modal1");
+const cancel1 = document.querySelectorAll(".cancel2");
+
+// Event listener for addAddress element
+search.addEventListener("click", function() {
+    overlay1.classList.remove("hidden1");
+    modal1.classList.remove("hidden1");
+    document.documentElement.style.overflow = 'hidden1';
+});
+
+// Event listener for cancel elements
+cancel.forEach(can => {
+    can.addEventListener("click", function() {
+        overlay1.classList.add("hidden1");
+        modal1.classList.add("hidden1");
+        document.documentElement.style.overflow = 'auto';
+    });
+});
