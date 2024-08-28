5c839f9930bafc4980e7543627247172d9a0cae0
Fuwad_Oladega <122299902+FuwadOladega@users.noreply.github.com>
2024-08-28 08:20:56 -0400

    search bar completed

--- Eagles_Shoe_Website/src/main/resources/static/Script/search.js
+++ Eagles_Shoe_Website/src/main/resources/static/Script/search.js
@@ -2,12 +2,18 @@ const search = document.querySelector("#searchbar");
 const overlay1 = document.querySelector(".overlay1");
 const modal1 = document.querySelector(".modal1");
 const cancel1 = document.querySelectorAll(".cancel2");
+const empty = document.querySelector("#empty");
+const e = document.querySelector("#bigSerch");
 
 // Event listener for addAddress element
 search.addEventListener("click", function() {
     overlay1.classList.remove("hidden1");
     modal1.classList.remove("hidden1");
     document.documentElement.style.overflow = 'hidden';
+});
+
+empty.addEventListener("click", () => {
+    e.value = "";
 });
 
 // Event listener for cancel elements
