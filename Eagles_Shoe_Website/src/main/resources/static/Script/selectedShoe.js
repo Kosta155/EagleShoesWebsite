24792e34fc8a6b2142acbc25aba3599657a9845e
Fuwad_Oladega <122299902+FuwadOladega@users.noreply.github.com>
2024-09-03 19:47:36 -0400

    selectedShoe page drop downs completed

--- Eagles_Shoe_Website/src/main/resources/static/Script/selectedShoe.js
+++ Eagles_Shoe_Website/src/main/resources/static/Script/selectedShoe.js
@@ -52,3 +52,30 @@ let currentImage = 0;
         slideShowImages[index].classList.remove("hidden");
         tinyImages[index].style.opacity = "1";
     }
+    
+    
+    const dropdown = document.querySelectorAll(".product-details-dropdown");
+    const control = document.querySelectorAll(".product-details-control");
+    const icon = document.querySelectorAll(".desc-icons");
+   
+    
+    control.forEach((element,index) =>{	
+	 element.addEventListener("click", function ()
+	 {
+		 
+		 if(dropdown[index].classList.contains("dropdown-hidden"))
+		 {
+			 dropdown[index].classList.remove("dropdown-hidden");
+		     icon[index].classList.remove("fa-angle-down");
+		     icon[index].classList.add("fa-angle-up");
+		 }
+		 else
+		 {
+			 
+			 dropdown[index].classList.add("dropdown-hidden");
+		     icon[index].classList.add("fa-angle-down");
+		     icon[index].classList.remove("fa-angle-up"); 
+		 }
+	 })
+	 
+	 });
